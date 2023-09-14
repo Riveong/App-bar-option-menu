08fcd3e1da02b630b17702bfdbd715eb7d91fad2
Riveong <setsubi9@gmail.com>
2023-09-14 15:35:58 +0700

    adding search bar

--- app/src/main/java/com/riveong/navbar/MainActivity.kt
+++ app/src/main/java/com/riveong/navbar/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_main)
+
 
 
         binding.topAppBar.setOnMenuItemClickListener { menuItem ->
--- app/src/main/java/com/riveong/navbar/MenuActivity.kt
+++ app/src/main/java/com/riveong/navbar/MenuActivity.kt
@@ -2,10 +2,42 @@ package com.riveong.navbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.search.SearchBar
+import com.riveong.navbar.databinding.ActivityMainBinding
+import com.riveong.navbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMenuBinding
+    private lateinit var searchBar: SearchBar
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+
+
+
+
+        with(binding){//logic when clicked
+            searchView.setupWithSearchBar(searchBar) //bind them
+            searchView.editText.setOnEditorActionListener{ textView, actionId, event ->
+                searchBar.text = searchView.text
+                searchView.hide()
+                Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show() //some logic example here
+                false
+
+
+            }
+        }
+
     }
 }
